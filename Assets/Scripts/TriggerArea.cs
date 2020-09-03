4c29b0e61c28bae5236dc7b1ab2a2e290019077c
hae <haedesu902@gmail.com>
2020-09-03 16:36:40 +0900

    Added TriggerArea.

---
+++ Assets/Scripts/TriggerArea.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class TriggerArea : MonoBehaviour
+{
+	private List<GameObject> gameObjectsInTrigger = new List<GameObject>();
+
+	private void Start()
+	{
+		GetComponent<Collider>().isTrigger = true;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		gameObjectsInTrigger.Add(other.gameObject);
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		gameObjectsInTrigger.Remove(other.gameObject);
+	}
+
+	private void Update()
+	{
+		gameObjectsInTrigger.RemoveAll(item => (item == null) || (!item.activeSelf));
+		 Debug.LogFormat("Count: {0}", gameObjectsInTrigger.Count);
+	}
+}
